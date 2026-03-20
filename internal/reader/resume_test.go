2d7119f8c29e8c98d97412b2ade9fd72b124ae58
povarna <povarna@adobe.com>
2026-03-20 20:21:55 +0200

    Adding tests

--- internal/reader/resume_test.go
+++ internal/reader/resume_test.go
@@ -29,5 +29,38 @@ func TestResumeLoad(t *testing.T) {
 		t.Fatal("Unable to run Reader Load function")
 	}
 
+	assert.Len(t, d, 2)
 	assert.Equal(t, d["c1"], true)
+	assert.Equal(t, d["c2"], true)
+
+}
+
+func TestResumeLoad_InvalidJson(t *testing.T) {
+	f, err := os.CreateTemp("", "stamper-*.jsonl")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(f.Name())
+	f.WriteString("invalid json")
+	f.Close()
+
+	logger := zerolog.Nop()
+	resume := NewResume(&logger)
+
+	_, err = resume.Load(f.Name())
+
+	if err == nil {
+		t.Fatal("Expected error for invalid json.")
+	}
+}
+
+func TestReaderLoad_FileNotExist(t *testing.T) {
+	logger := zerolog.Nop()
+	reader := NewResume(&logger)
+
+	d, err := reader.Load("inexistentFile.txt")
+
+	assert.NoError(t, err)
+	assert.Empty(t, d)
 }
